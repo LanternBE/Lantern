9fa1b87d8396189a4affc4999aca7d4a82e5043e
cheditx <christianbuonpensiere222@gmail.com>
2025-06-24 01:58:54 +0200

    test

--- Lantern/Program.cs
+++ Lantern/Program.cs
@@ -1,3 +1,16 @@
-﻿using System;
+﻿using System.Net;
+using System.Threading.Tasks;
+using RakSharp;
 
-Console.WriteLine("Hello, World!");
+namespace Lantern;
+
+public static class Program {
+    
+    public async static Task Main() {
+
+        var server = new Server(new IPEndPoint(IPAddress.Any, 19132));
+        await server.Start();
+        
+        // test
+    }
+}
