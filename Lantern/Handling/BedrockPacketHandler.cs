2e8ef3b56a55746f56beffd1d8c5684b57bdd786
cheditx <christianbuonpensiere222@gmail.com>
2025-06-26 03:33:34 +0200

    Need to fix SendBedrockPacketAsync()

--- Lantern/Handling/BedrockPacketHandler.cs
+++ Lantern/Handling/BedrockPacketHandler.cs
@@ -1,6 +1,11 @@
 using System.Net;
 using System.Net.Sockets;
+using BedrockProtocol;
+using BedrockProtocol.Types;
 using RakSharp;
+using RakSharp.Protocol;
+using RakSharp.Protocol.Online;
+using RakSharp.Utils;
 
 namespace Lantern.Handling;
 
@@ -22,4 +27,48 @@ public abstract class BedrockPacketHandler<T> {
     }
 
     public abstract Task<bool> HandleAsync();
+    
+    protected async Task SendEncapsulatedPacketAsync((EncapsulatedPacket packet, byte[] buffer) response) {
+        
+        var session = Server.SessionsManager.GetSession(ClientEndPoint);
+        if (session is null) {
+            Logger.LogError($"Session not found for the client endpoint ({ClientEndPoint})");
+            return;
+        }
+        
+        var datagram = Datagram.Create(0, [response.packet], session.GetNextSequenceNumber());
+        await Socket.SendToAsync(datagram.buffer, SocketFlags.None, ClientEndPoint);
+    }
+    
+    protected async Task SendBedrockPacketAsync((BedrockPacket packet, byte[] buffer) response) {
+
+        var tempBuffer = new byte[1492];
+        var writer = new RakSharp.Binary.BinaryWriter(tempBuffer);
+        
+        response.packet.Write(writer);
+        var bedrockBuffer = new byte[writer.Position];
+        
+        Array.Copy(tempBuffer, bedrockBuffer, writer.Position);
+        var (gamePacket, gamePacketBuffer) = GamePacket.Create(
+            subClientId: 0,
+            subTargetId: 0,
+            payload: bedrockBuffer
+        );
+        
+        Console.WriteLine($"Buffer gamepacket: {BitConverter.ToString(gamePacketBuffer)}");
+        
+        var session = Server.SessionsManager.GetSession(ClientEndPoint);
+        if (session == null)
+            throw new InvalidOperationException($"Session not found for {ClientEndPoint}");
+        
+        var encapsulatedPacket = EncapsulatedPacket.Create(gamePacketBuffer, PacketReliability.Reliable, session.GetNextReliableIndex(), session.GetNextOrderedIndex());
+        Console.WriteLine($"Buffer encapsulatedpacket: {BitConverter.ToString(encapsulatedPacket.Buffer)}");
+        var sequenceNumber = session.GetNextSequenceNumber();
+        
+        var datagram = Datagram.Create(0, [encapsulatedPacket], sequenceNumber);
+        Console.WriteLine($"Buffer datagram: {BitConverter.ToString(datagram.buffer)}");
+        session.TrackReliablePacket(sequenceNumber, datagram);
+        
+        await Socket.SendToAsync(datagram.buffer, SocketFlags.None, ClientEndPoint);
+    }
 }
--- Lantern/Handling/Handlers/RequestNetworkSettingsHandler.cs
+++ Lantern/Handling/Handlers/RequestNetworkSettingsHandler.cs
@@ -1,5 +1,7 @@
 using BedrockProtocol;
+using BedrockProtocol.Types;
 using RakSharp.Utils;
+using PlayStatus = BedrockProtocol.PlayStatus;
 
 namespace Lantern.Handling.Handlers;
 
@@ -7,9 +9,21 @@ public class RequestNetworkSettingsHandler : BedrockPacketHandler<RequestNetwork
     
     public override async Task<bool> HandleAsync() {
         
-        Logger.LogInfo("Received request network settings!");
-        Console.WriteLine(Packet.ProtocolVersion);
-        Console.WriteLine(Packet.CompressionAlgorithm);
+        Logger.LogDebug($"Received RequestNetworkSettings inside the GamePacket from ({ClientEndPoint}) with Protocol Version: {Packet.ProtocolVersion}");
+        /*if (Packet.ProtocolVersion < Info.SupportedProtocols.Last()) {
+            Logger.LogError($"Protocol version {Packet.ProtocolVersion} is outdated");
+            return false;
+        }
+
+        if (Packet.ProtocolVersion > Info.SupportedProtocols.Last()) {
+            Logger.LogError($"Server Protocol version {Packet.ProtocolVersion} is outdated");
+            return false;
+        }*/
+
+        var playStatus = PlayStatus.Create(Compression.Algorithm.Zlib, BedrockProtocol.Types.PlayStatus.LoginFailedServerOld);
+        Console.WriteLine($"Buffer playstatus: {BitConverter.ToString(playStatus.buffer)}");
+        await SendBedrockPacketAsync(playStatus);
+        
         return true;
     }
 }
